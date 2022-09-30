3144b92adf0e3475e67b20dde13c2971ae82d305
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2022-09-30 20:27:32 +0700

    Add modal DOM

--- src/scripts/submitBtn.js
+++ src/scripts/submitBtn.js
@@ -5,7 +5,52 @@ function openModal() {
     setTimeout(showModal, 2000);
 
     function showModal() {
-        const modal = document.querySelector(".modal-window")
-        modal.style.display = "block";
+        const modal = document.createElement("div");
+        modal.classList.add("modal-window");
+
+        const modalWrapper = document.createElement("div")
+        modalWrapper.classList.add("modal-wrapper");
+        modalWrapper.classList.add("text-center");
+
+        const modalHeader = document.createElement("div");
+        modalHeader.classList.add("modal-header");
+
+        const icon = document.createElement("i");
+        icon.classList.add("fa");
+        icon.classList.add("fa-check-circle");
+        icon.classList.add("fa-5x");
+        icon.classList.add("icon-check");
+
+        const heading = document.createElement("h4");
+        heading.innerText = "Upps, ada yang salah";
+
+        modalHeader.append(icon, heading);
+
+        const modalBody = document.createElement("div");
+        modalBody.classList.add("modal-body");
+
+        const text = document.createElement("p");
+        text.innerText = "Mungkin ada email atau passwordmu yang tidak valid, ulangi sekali lagi.";
+
+        const btn = document.createElement("a");
+        btn.setAttribute("href", "dashboard.html");
+        btn.classList.add("btn");
+        btn.classList.add("btn-primary");
+        btn.classList.add("btn-lg");
+        btn.classList.add("btn-dashboard");
+        btn.classList.add("shadow-sm");
+        btn.innerText = "Dashboard";
+
+        modalBody.append(text, btn);
+        
+        modalWrapper.append(modalHeader, modalBody);
+
+        modal.append(modalWrapper);
+
+        document.body.appendChild(modal);
+
+        modal.addEventListener("click", function() {
+            modal.style.display = "none";
+        })
     }
 }
