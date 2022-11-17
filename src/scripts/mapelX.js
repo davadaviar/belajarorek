1b3d756ea16b87cd5dc58152f1f3815e349972d1
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2022-11-17 15:59:37 +0700

    Add ddpk

--- src/scripts/mapelX.js
+++ src/scripts/mapelX.js
@@ -2,4 +2,5 @@ const mapelX = document.getElementById("mapel__x");
 
 mapelX.addEventListener("click", function() {
     window.location.href = "mapelX.html";
+    // console.log("Ini mapel kelas X");
 })
